2fc3e34650daa9e10efa59fbaab5898689e99c12
mjavadf <javad.me1376@gmail.com>
2023-02-11 14:35:34 +0100

    First Commit

---
+++ models/entity_with_metadata_models.py
@@ -0,0 +1,16 @@
+from .main_models import IdentifiableEntity
+
+class EntityWithMetadata(IdentifiableEntity):
+    pass
+
+
+class Collection(EntityWithMetadata):
+    pass
+
+
+class Manifest(EntityWithMetadata):
+    pass
+
+
+class Canvas(EntityWithMetadata):
+    pass
---
+++ models/main_models.py
@@ -0,0 +1,10 @@
+class IdentifiableEntity(object):
+    pass
+
+
+class Annotation(IdentifiableEntity):
+    pass
+
+
+class Image(IdentifiableEntity):
+    pass

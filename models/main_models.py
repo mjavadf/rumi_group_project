b0105ef427eebc9e637150da0ef3c72dfed242c8
mjavadf <javad.me1376@gmail.com>
2023-02-11 14:57:21 +0100

    Modify main models

--- models/main_models.py
+++ models/main_models.py
@@ -1,10 +1,27 @@
 class IdentifiableEntity(object):
-    pass
-
-
-class Annotation(IdentifiableEntity):
-    pass
+    def __init__(self, id: str) -> None:
+        self.id = id
+    
+    def getId(self) -> str:
+        return self.id
 
 
 class Image(IdentifiableEntity):
     pass
+
+
+class Annotation(IdentifiableEntity):
+    def __init__(self, id: str, motivation: str, target: IdentifiableEntity, body: Image) -> None:
+        self.motivation = motivation
+        self.target = target
+        self.body = body
+        super().__init__(id)
+    
+    def getBody(self) -> Image:
+        return self.body
+    
+    def getTarget(self) -> IdentifiableEntity:
+        return self.target
+    
+    def getMotivation(self) -> str:
+        return self.motivation

9b5e3382d5b16bf573ca2c2da9f2809e993315ed
mjavadf <javad.me1376@gmail.com>
2023-03-29 16:15:39 +0200

    add GenericQueryProcessor to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from models.main_models import IdentifiableEntity
 
 
 class Processor(object):
@@ -84,4 +85,71 @@ class RelationalQueryProcessor(QueryProcessor):
         pass
 
 
+class GenericQueryProcessor(object):
+    def __init__(self, query_processor: QueryProcessor) -> None:
+        self.query_processor = query_processor
 
+    def clean_query_processor(self) -> bool:
+        pass
+
+    def add_query_processor(self, processor: QueryProcessor) -> bool:
+        pass
+
+    def get_all_annotations(self) -> list:
+        pass
+
+    def get_all_canvas(self) -> list:
+        pass
+
+    def get_all_collections(self) -> list:
+        pass
+
+    def get_all_images(self) -> list:
+        pass
+
+    def get_all_manifests(self) -> list:
+        pass
+
+    def get_annotations_to_canvas(self, canvas_id: str) -> list:
+        pass
+
+    def get_annotations_to_collection(self, collection_id: str) -> list:
+        pass
+
+    def get_annotations_to_manifest(self, manifest_id: str) -> list:
+        pass
+
+    def get_annotations_with_body(self, body_id: str) -> list:
+        pass
+
+    def get_annotations_with_body_and_target(self,
+                                             body_id: str,
+                                             target_id: str) -> list:
+        pass
+
+    def get_annotations_with_target(self, target_id: str) -> list:
+        pass
+
+    def get_canvases_in_collection(self, collection_id: str) -> list:
+        pass
+
+    def get_canvases_in_manifest(self, manifest_id: str) -> list:
+        pass
+
+    def get_entity_by_id(self, entity_id: str) -> IdentifiableEntity:
+        pass
+
+    def get_entities_with_creator(self, creator_name: str) -> list:
+        pass
+
+    def get_with_label(self, label: str) -> list:
+        pass
+
+    def get_entities_with_title(self, title: str) -> list:
+        pass
+
+    def get_images_annotating_canvas(self, canvas_id: str) -> list:
+        pass
+
+    def get_manifests_in_collection(self, collection_id: str) -> list:
+        pass

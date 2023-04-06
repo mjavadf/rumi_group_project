0e863fc2462e5025a47839681ce0739f2a0b3c2c
t-sniezek <124570541+t-sniezek@users.noreply.github.com>
2023-04-06 22:31:55 +0200

    Update main_models.py

--- models/main_models.py
+++ models/main_models.py
@@ -75,3 +75,57 @@ class Annotation(IdentifiableEntity):
         :rtype: str
         """
         return self.motivation
+    
+    class EntityWithMetaData(IdentifiableEntity):
+    """
+    A subclass of the IdentifiableEntity class that represents the MetaData of the Entity with a unique identifier, a lable, a title and creators.
+    """
+
+    def __init__(self, id: str, lable: str, title: str or none, creators: str) -> None:
+        """
+        Initialize an instance of the class with an identifier, a label, a title, and creators.
+
+        :param id: A unique identifier for the entity.
+        :type id: str
+        :param lable: The label for the Entity
+        :type lable: str
+        :param title: The title of the Entity
+        :type title: str or none
+        :param creators: the creators of the Entity
+        :type creators: str
+        """
+        self.label = label
+        self.title = title
+        self.creators = creators
+        super().__init__(id)
+        
+    def getLable(self) -> str:
+        """
+        Get the label of the entity.
+
+        :return: The label of the Entity
+        :rtype: str
+        """
+        return self.label
+
+    def getTitle(self) -> str:
+        """
+        Get the title of the entity.
+
+        :return: The title of the entity
+        :rtype: str or none
+        """
+        if title not in self.title:
+            return "none"
+        else:
+            return self.title
+        
+    def getCreators(self) -> str:
+        """
+        Get the creators of the entity.
+
+        :return: The creators of teh entity
+        :rtype: str
+        """
+        return self.creators
+

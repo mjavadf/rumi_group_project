e16b6cf81044fb15e679f01950b1b72526ecbe58
mjavadf <javad.me1376@gmail.com>
2023-06-04 19:23:22 +0200

    fix main_models.py

--- models/main_models.py
+++ models/main_models.py
@@ -75,73 +75,45 @@ class Annotation(IdentifiableEntity):
         :rtype: str
         """
         return self.motivation
-    
+
+
 class EntityWithMetaData(IdentifiableEntity):
-    """
-    A subclass of the IdentifiableEntity class that represents the MetaData of the Entity with a unique identifier, a lable, a title and creators.
-    """
-
-    def __init__(self, id: str, label: str, title: str, creators: str) -> None:
-        """
-        Initialize an instance of the class with an identifier, a label, a title, and creators.
-
-        :param id: A unique identifier for the entity.
-        :type id: str
-        :param lable: The label for the Entity
-        :type lable: str
-        :param title: The title of the Entity
-        :type title: str or none
-        :param creators: the creators of the Entity
-        :type creators: str
-        """
+    def __init__(self, id, label, title=None, creators=None) -> None:
+        if creators is None:
+            creators = []
         self.label = label
         self.title = title
         self.creators = creators
         super().__init__(id)
-        
-    def getLable(self) -> str:
-        """
-        Get the label of the entity.
 
-        :return: The label of the Entity
-        :rtype: str
-        """
+    def getLabel(self) -> str:
         return self.label
 
     def getTitle(self) -> str:
-        """
-        Get the title of the entity.
+        return self.title
 
-        :return: The title of the entity
-        :rtype: str or none
-        """
-        if title not in self.title:
-            return "none"
-        else:
-            return self.title
-        
     def getCreators(self) -> str:
-        """
-        Get the creators of the entity.
-
-        :return: The creators of teh entity
-        :rtype: str
-        """
         return self.creators
-    
+
+
 class Collection(EntityWithMetaData):
-        
-    def __init__(self, items):
+
+    def __init__(self, items, id, label):
+        super().__init__(id, label)
         self.items = items
+
     def getItems(self):
         return self.items
-    
+
+
 class Manifest(EntityWithMetaData):
-    def __init__(self, items):
+    def __init__(self, items, id, label):
+        super().__init__(id, label)
         self.items = items
+
     def getItems(self):
         return self.items
-    
+
+
 class Canvas(EntityWithMetaData):
     pass
-
